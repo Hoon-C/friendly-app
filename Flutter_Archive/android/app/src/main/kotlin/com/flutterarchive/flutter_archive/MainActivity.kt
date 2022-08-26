d990da03343aaf653898fc6e9c8ca9ac600ec850
seunggyundev <91270887+seunggyundev@users.noreply.github.com>
2022-08-27 03:32:11 +0900

    test

---
+++ Flutter_Archive/android/app/src/main/kotlin/com/flutterarchive/flutter_archive/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutterarchive.flutter_archive
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ test_hosting/android/app/src/main/kotlin/com/test_hosting/test_hosting/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.test_hosting.test_hosting
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
